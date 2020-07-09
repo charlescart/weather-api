7b81db28c51b08026b7da9dc2de3c411029ea868
Charles <charles26.dev@gmail.com>
2020-07-08 21:53:34 -0300

    testing detection ip 3

--- src/features/weather/WeatherController.ts
+++ src/features/weather/WeatherController.ts
@@ -1,18 +1,24 @@
 /* eslint-disable no-console */
 /* eslint-disable class-methods-use-this */
 import {
-  JsonController, Get, Req,
+  JsonController, Get, Req, HeaderParam, UseBefore,
 } from 'routing-controllers';
 import { Request } from 'express';
+import requestIp from 'request-ip';
 // import Soup from './validators/soupDto';
 // import SoupRepository from './SoupRepository';
 import WeatherRepository from './WeatherRepository';
 
 @JsonController()
+@UseBefore(requestIp.mw())
 export default class AuthController {
   @Get('/location')
-  location(@Req() req: Request): unknown {
-    console.log(`Ip Cliente: ${req.connection}`);
+  location(@Req() req: Request, @HeaderParam('x-forwarded-for') ipA: string): unknown {
+    console.log(req.connection.remoteAddress);
+    console.log(`x-forwarded-for: ${ipA}`);
+    console.log(`ip: ${req.ip}`);
+    console.log(`ips: ${req.ips}`);
+    console.log(`ip library: ${req.clientIp}`);
     const userIp: string | undefined = req.connection.remoteAddress;
     return WeatherRepository.location(userIp);
   }
--- src/features/weather/WeatherRepository.ts
+++ src/features/weather/WeatherRepository.ts
@@ -4,7 +4,7 @@ import publicIp from 'public-ip';
 export default class WeatherRepository {
   static async location(userIp: string | undefined): any {
     const serverIp = await publicIp.v4();
-    console.log(`Ip server: ${userIp}`);
+    console.log(`Ip server: ${serverIp}`);
     return { userIp, serverIp };
   }
 }
